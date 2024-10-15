3dce21d752b9caf7213f7a78165c12413cb41ea2
SyedBashar <MhuammadBashar@gmail.com>
2024-10-15 18:08:10 +0500

    Task 2 Done weather

---
+++ app.js
@@ -0,0 +1,14 @@
+var weather = prompt("Put Weather Conditions ").toLowerCase();
+
+if (weather == "sunny") {
+    console.log("Wear sunglasses!");
+} 
+else if (weather == "rainy") {
+    console.log("Take an umbrella!");
+} 
+else if (weather == "snowy") {
+    console.log("Wear warm clothes!");
+} 
+else {
+    console.log("Enjoy your day!");
+}
